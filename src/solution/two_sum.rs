289c3fdd04d4249792502be001c3ef1a9c147470
Arano <aranoverse@outlook.com>
2022-08-29 20:49:10 +0800

    refactor(`two_sum`): beautify code with `Some` operator

--- src/solution/two_sum.rs
+++ src/solution/two_sum.rs
@@ -5,19 +5,18 @@ impl Solution {
     //! https://leetcode.cn/problems/two-sum/
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut indexies: Vec<i32> = Vec::with_capacity(2);
-        let mut num_to_index: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
+        let mut num_to_index = HashMap::with_capacity(nums.len());
 
-        for i in 0..nums.len() {
-            let num = nums[i];
+        for (idx, num) in nums.iter().enumerate() {
             let left = target - num;
 
-            if num_to_index.contains_key(&left) {
-                indexies.insert(0, i as i32);
-                indexies.insert(1, *num_to_index.get(&left).unwrap());
+            if let Some(left_index) = num_to_index.get(&left) {
+                indexies.insert(0, idx as i32);
+                indexies.insert(1, *left_index);
                 break;
             }
 
-            num_to_index.insert(num, i as i32);
+            num_to_index.insert(*num, idx as i32);
         }
 
         indexies
