1d6887f4b8c0e222e66360526f2d5892cb675499
Arano <aranoverse@outlook.com>
2022-08-30 14:16:52 +0800

    feat(climb_stairs): add `climb_stairs` solution
    https://leetcode.com/problems/climbing-stairs

---
+++ src/solution/climb_stairs.rs
@@ -0,0 +1,48 @@
+use std::process::id;
+use crate::solution::Solution;
+
+impl Solution {
+    pub fn climb_stairs_recurse(n: i32) -> i32 {
+        if 0 <= n && n <= 2 {
+            return n;
+        }
+
+        Self::climb_stairs(n - 1) + Self::climb_stairs(n - 2)
+    }
+
+    pub fn climb_stairs(n: i32) -> i32 {
+        if n < 3 {
+            return n;
+        }
+
+        let mut step_ways = Vec::with_capacity(n as usize + 1);
+
+        for idx in 0..step_ways.capacity() {
+            if idx < 3 {
+                step_ways.push(idx);
+                continue;
+            }
+
+            step_ways.push(step_ways[idx - 1] + step_ways[idx - 2]);
+        }
+
+
+        step_ways[n as usize] as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solution::Solution;
+    use test_case::test_case;
+
+
+    #[test_case(2, 2;)]
+    #[test_case(3, 3;)]
+    #[test_case(4, 5;)]
+    fn test_climb_stairs(steps: i32, ways: i32) {
+        assert_eq!(Solution::climb_stairs(steps), ways);
+    }
+}
+
+
--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1,4 +1,5 @@
 mod two_sum;
 mod is_palindrome;
+mod climb_stairs;
 
-pub struct Solution {}
+pub struct Solution;
